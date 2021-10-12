7ff9bd72e17fee0d61fe10ae21425f1444c80032
Leon8012 <85956044+Leon8012@users.noreply.github.com>
2021-10-12 23:43:25 +0300

    Update dictionar.js

--- dictionar.js
+++ dictionar.js
@@ -11,10 +11,23 @@ function search () {
 	let word_search = document.getElementById('search_word').value;
 	let finder = set.has(word_search); 
 	if (finder) {
-		document.getElementById('message').innerHTML = "THE WANTED WORD IS IN THE DICTIONARY";
+		document.getElementById('message').innerHTML = "THE WORD '" + word_search + "' IS IN THE DICTIONARY";
 	}else {
-		document.getElementById('message').innerHTML = "Unfortunately, the word is not yet in the dictionary";
+		document.getElementById('message').innerHTML = "Unfortunately, the word  '"  + word_search + "' is not yet in the dictionary";
 	}
 	document.getElementById('search_word').value = "";
 	return false;
+}
+
+function dictionary() {
+	let list = 'The list of words in the dictionary is: ';
+	if(set.size == 0) {
+		list += "emty";
+	} else {
+		set.forEach(word => {
+	  		list += word + ", ";
+		});
+	}	
+	document.getElementById('dictionary').innerHTML = list;
+	return false; 
 }
